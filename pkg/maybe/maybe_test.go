e7aeec9130d71c55b049388fe5fa6cb1a93f78df
Jaime Terreu <>
2024-06-04 09:42:30 +0930

    Port all maybe api to go

--- pkg/maybe/maybe_test.go
+++ pkg/maybe/maybe_test.go
@@ -48,6 +48,102 @@ func TestMap(t *testing.T) {
 			Map2(basics.Add[int], m1, m2),
 		)
 	})
+
+	t.Run("Map3 a Nothing", func(t *testing.T) {
+		var m1 Maybe[int]
+		var m2 Maybe[int]
+		var m3 Maybe[int]
+
+		m1 = Just[int]{Value: 22}
+		m2 = Just[int]{Value: 23}
+		m3 = Nothing{}
+
+		asserts.Equal(Nothing{},
+			Map3(func(_ int, _ int, _ int) int { return 1 }, m1, m2, m3),
+		)
+	})
+
+	t.Run("Map3 a Just", func(t *testing.T) {
+		var m1 Maybe[int]
+		var m2 Maybe[int]
+		var m3 Maybe[int]
+
+		m1 = Just[int]{Value: 1}
+		m2 = Just[int]{Value: 2}
+		m3 = Just[int]{Value: 3}
+
+		asserts.Equal(Just[int]{Value: 6},
+			Map3(func(a int, b int, c int) int { return basics.Add(basics.Add(a, b), c) }, m1, m2, m3),
+		)
+	})
+
+	t.Run("Map4 a Nothing", func(t *testing.T) {
+		var m1 Maybe[int]
+		var m2 Maybe[int]
+		var m3 Maybe[int]
+		var m4 Maybe[int]
+
+		m1 = Just[int]{Value: 22}
+		m2 = Just[int]{Value: 23}
+		m3 = Just[int]{Value: 23}
+		m4 = Nothing{}
+
+		asserts.Equal(Nothing{},
+			Map4(func(_ int, _ int, _ int, _ int) int { return 1 }, m1, m2, m3, m4),
+		)
+	})
+
+	t.Run("Map4 a Just", func(t *testing.T) {
+		var m1 Maybe[int]
+		var m2 Maybe[int]
+		var m3 Maybe[int]
+		var m4 Maybe[int]
+
+		m1 = Just[int]{Value: 1}
+		m2 = Just[int]{Value: 2}
+		m3 = Just[int]{Value: 3}
+		m4 = Just[int]{Value: 3}
+
+		asserts.Equal(Just[int]{Value: 22},
+			Map4(func(a int, b int, c int, d int) int { return 22 }, m1, m2, m3, m4),
+		)
+	})
+
+	t.Run("Map5 a Nothing", func(t *testing.T) {
+		var m1 Maybe[int]
+		var m2 Maybe[int]
+		var m3 Maybe[int]
+		var m4 Maybe[int]
+		var m5 Maybe[int]
+
+		m1 = Just[int]{Value: 22}
+		m2 = Just[int]{Value: 23}
+		m3 = Just[int]{Value: 23}
+		m4 = Just[int]{Value: 23}
+		m5 = Nothing{}
+
+		asserts.Equal(Nothing{},
+			Map5(func(_ int, _ int, _ int, _ int, _ int) int { return 1 }, m1, m2, m3, m4, m5),
+		)
+	})
+
+	t.Run("Map5 a Just", func(t *testing.T) {
+		var m1 Maybe[int]
+		var m2 Maybe[int]
+		var m3 Maybe[int]
+		var m4 Maybe[int]
+		var m5 Maybe[int]
+
+		m1 = Just[int]{Value: 1}
+		m2 = Just[int]{Value: 2}
+		m3 = Just[int]{Value: 3}
+		m4 = Just[int]{Value: 3}
+		m5 = Just[int]{Value: 3}
+
+		asserts.Equal(Just[int]{Value: 22},
+			Map5(func(a int, b int, c int, d int, e int) int { return 22 }, m1, m2, m3, m4, m5),
+		)
+	})
 }
 
 func TestAndThen(t *testing.T) {
